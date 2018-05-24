2a26de0ae8200d589affe1d781c17564e8f2cfa5
akshayajk <36322468+akshayajk@users.noreply.github.com>
2018-05-24 15:11:27 +0530

    Create 4.cpp

---
+++ set1/4.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std; 
+int main ()
+{
+  int n,count;
+  cin>>n;
+  int a[n],i,j;
+  for(i=0;i<n;i++)
+  {
+      cin>>a[i];
+  }
+  for(i=0;i<n;i++)
+  {
+      count=0;
+      for(j=i+1;j<n;j++)
+      {
+          if(a[i]==a[j])
+          {
+              count=count+1;
+          }
+      }
+      if(count==0)
+      {
+          cout<<a[i];
+          break;
+      }
+     }
+
+    return 0;
+}
